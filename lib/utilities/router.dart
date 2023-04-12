bfc310d636c495d22d7ec50018f97a3ff5c8546f
Mohamed abo el maaty zaky <98196426+MohamedAboElM3aTy@users.noreply.github.com>
2023-04-12 22:28:36 +0200

    onGenerateRoute: Start working on  it

---
+++ lib/utilities/router.dart
@@ -0,0 +1,32 @@
+// import 'package:flutter/material.dart';
+
+// import 'package:wed_plan_project/views/pages/intro_screen.dart';
+// import 'package:wed_plan_project/views/pages/signIn/login_screen.dart';
+// import 'package:wed_plan_project/views/pages/signUp/signUp_screen.dart';
+// import 'package:wed_plan_project/views/pages/welcome_screen.dart';
+// import './routes.dart';
+
+// Route<dynamic> generateRoute(RouteSettings settings) {
+//   switch (settings.name) {
+//     case '/':
+//       return MaterialPageRoute(
+//         builder: (_) => WelcomeScreen(),
+//       );
+//     case '/login':
+//       return MaterialPageRoute(
+//         builder: (_) => LoginScreen(),
+//       );
+//     case '/home':
+//       return MaterialPageRoute(
+//         builder: (_) => IntroScreen(),
+//       );
+//     case '/signUp':
+//       return MaterialPageRoute(
+//         builder: (_) => SignUp(),
+//       );
+//     default:
+//       return MaterialPageRoute(
+//         builder: (_) => WelcomeScreen(),
+//       );
+//   }
+// }
---
+++ lib/utilities/routes.dart
@@ -0,0 +1,6 @@
+// class AppRoutes {
+//   static const String welcomeScreenRoute = '/';
+//   static const String loginPageRoute = '/login';
+//   static const String introScreenRoute = '/home';
+//   static const String signUpScreenRoute = '/signUp';
+// }
