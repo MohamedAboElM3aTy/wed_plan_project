11ac238fb814610d5c2df81037049a9b397f29c8
Mohamed abo el maaty zaky <98196426+MohamedAboElM3aTy@users.noreply.github.com>
2023-04-27 04:49:06 +0200

    Start adding dummy data

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,40 @@
+import 'package:wed_plan_project/models/category.dart';
+
+List availableCategories = [
+  Category(
+    title: 'Wedding Planner',
+    imageUrl: 'assets/images/Images/wedding_planner.jpg',
+  ),
+  Category(
+    title: 'Photographer',
+    imageUrl: 'assets/images/Images/photographer.jpg',
+  ),
+  Category(
+    title: 'Makeup Artist',
+    imageUrl: 'assets/images/Images/makeup-artist.avif',
+  ),
+  Category(
+    title: 'Veil Designer',
+    imageUrl: 'assets/images/Images/veil-designer.jpg',
+  ),
+  Category(
+    title: 'Hair Dresser',
+    imageUrl: 'assets/images/Images/Hair-dresser.avif',
+  ),
+  Category(
+    title: 'Head Piece',
+    imageUrl: 'assets/images/Images/head-piece.avif',
+  ),
+  Category(
+    title: 'Nails',
+    imageUrl: 'assets/images/Images/nails.jpg',
+  ),
+  Category(
+    title: 'Fashion Designer',
+    imageUrl: 'assets/images/Images/Fashion-designer.jpg',
+  ),
+  Category(
+    title: 'Jewelry',
+    imageUrl: 'assets/images/Images/jewellery.avif',
+  ),
+];
