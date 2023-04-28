1bd5b67893c05e7b7fd91a159b0f080f09bf33dc
Mohamed abo el maaty zaky <98196426+MohamedAboElM3aTy@users.noreply.github.com>
2023-04-28 11:15:17 +0300

    Edit the attributes of the class

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -1,15 +1,11 @@
-import 'package:flutter/material.dart';
-import 'package:uuid/uuid.dart';
-
-const uuid = Uuid();
-
 class Category {
   final String title;
   final String id;
   final String imageUrl;
 
-  Category({
+  const Category({
     required this.title,
     required this.imageUrl,
-  }) : id = uuid.v4();
+    required this.id,
+  });
 }
---
+++ lib/models/vendor.dart
@@ -0,0 +1,18 @@
+class Vendor {
+  final String name;
+  final String id;
+  final String category;
+
+  final String imageUrl;
+  final String businessUrl;
+  final String phoneNumber;
+
+  const Vendor({
+    required this.name,
+    required this.id,
+    required this.category,
+    required this.imageUrl,
+    required this.businessUrl,
+    required this.phoneNumber,
+  });
+}
