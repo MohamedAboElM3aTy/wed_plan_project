78326bc7b9f990eea954d59da09aefb4642e193c
Mohamed abo el maaty zaky <98196426+MohamedAboElM3aTy@users.noreply.github.com>
2023-05-10 15:43:09 +0300

    MainDrawer but navigation doesnot work

--- lib/views/widgets/main_drawer.dart
+++ lib/views/widgets/main_drawer.dart
@@ -1,102 +1,84 @@
+// required this.onSelectScreen,
+// final void Function(String identifier) onSelectScreen;
 import 'package:flutter/material.dart';
-
-import 'package:wed_plan_project/utilities/constants.dart';
 import 'package:wed_plan_project/services/auth.dart';
+import 'package:wed_plan_project/utilities/constants.dart';
 import 'package:wed_plan_project/utilities/routes.dart';
+import './drawer_list_tile.dart';
 
 class MainDrawer extends StatelessWidget {
   final Auth auth = Auth();
-  MainDrawer({
-    super.key,
-    // required this.onSelectScreen,
-  });
-
-  // final void Function(String identifier) onSelectScreen;
 
   @override
   Widget build(BuildContext context) {
     return Drawer(
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          DrawerHeader(
-            padding: const EdgeInsets.all(15),
+          Container(
+            height: 200,
+            width: double.infinity,
             decoration: BoxDecoration(
               gradient: LinearGradient(
                 colors: [
-                  Colors.purpleAccent,
                   kColorScheme.primary,
+                  Colors.purpleAccent,
                 ],
                 begin: Alignment.topLeft,
                 end: Alignment.bottomRight,
               ),
             ),
-            child: Row(
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Icon(
+                    Icons.menu_open_outlined,
+                    size: 48,
+                    color: kColorScheme.secondary,
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Menu',
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.onSecondaryContainer,
+                      fontSize: 30,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          Expanded(
+            child: Column(
               children: [
-                Icon(
-                  Icons.menu,
-                  size: 48,
-                  color: kColorScheme.secondary,
+                BuildListTile(
+                  icon: Icons.person_3,
+                  titleText: 'Vendors',
+                  onTap: () {},
                 ),
-                const SizedBox(width: 20),
-                Text(
-                  'Menu',
-                  style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                        color:
-                            Theme.of(context).colorScheme.onSecondaryContainer,
-                        fontSize: 24,
-                      ),
+                Divider(),
+                BuildListTile(
+                  icon: Icons.shop_2_outlined,
+                  titleText: 'Cart',
+                  onTap: () =>
+                      Navigator.of(context).pushNamed(AppRoutes.cartScreen),
+                ),
+                Divider(),
+                BuildListTile(
+                  icon: Icons.logout,
+                  titleText: 'Log Out',
+                  onTap: () async {
+                    await auth.logout();
+                    Navigator.of(context)
+                        .pushReplacementNamed(AppRoutes.loginPage);
+                  },
                 ),
               ],
             ),
-          ),
-          ListTile(
-            leading: Icon(
-              Icons.view_agenda_rounded,
-              size: 26,
-              color: Theme.of(context).primaryColor,
-            ),
-            title: Text(
-              'Vendors',
-              style: Theme.of(context).textTheme.titleSmall!.copyWith(
-                    color: Theme.of(context).colorScheme.error,
-                    fontSize: 24,
-                  ),
-            ),
-            onTap: () {},
-          ),
-          const SizedBox(height: 10.0),
-          ListTile(
-            leading: Icon(
-              Icons.shop,
-              size: 26,
-              color: Theme.of(context).colorScheme.primary,
-            ),
-            title: Text(
-              'Cart',
-              style: Theme.of(context).textTheme.titleSmall!.copyWith(
-                    color: Theme.of(context).colorScheme.secondaryContainer,
-                    fontSize: 24,
-                  ),
-            ),
-            onTap: () {},
-          ),
-          ListTile(
-            leading: Icon(
-              Icons.logout,
-              size: 26,
-              color: Theme.of(context).primaryColor,
-            ),
-            title: Text(
-              'Log Out',
-              style: Theme.of(context).textTheme.titleSmall!.copyWith(
-                    color: Theme.of(context).colorScheme.error,
-                    fontSize: 24,
-                  ),
-            ),
-            onTap: () async {
-              await auth.logout();
-              Navigator.of(context).pushReplacementNamed(AppRoutes.loginPage);
-            },
           ),
         ],
       ),
