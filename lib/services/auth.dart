257d88dcd56ff04e95d3557f3016abc008301da3
Mohamed abo el maaty zaky <98196426+MohamedAboElM3aTy@users.noreply.github.com>
2023-05-10 15:40:22 +0300

    Add checkEmailExists feature

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -2,12 +2,14 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 abstract class AuthBase {
   User? get currentUser;
-  
+
   Future<User?> registerWithEmailAndPassword(String email, String password);
 
   Future<User?> loginWithEmailAndPassword(String email, String password);
 
   Future<void> logout();
+
+  Future<bool> checkEmailExists(String email);
 }
 
 class Auth implements AuthBase {
@@ -17,8 +19,8 @@ class Auth implements AuthBase {
   Future<User?> registerWithEmailAndPassword(
       String email, String password) async {
     try {
-      final userAuth = await _firebaseAuth
-          .createUserWithEmailAndPassword(email: email, password: password);
+      final userAuth = await _firebaseAuth.createUserWithEmailAndPassword(
+          email: email, password: password);
       return userAuth.user;
     } catch (e) {
       print(
@@ -31,8 +33,8 @@ class Auth implements AuthBase {
   @override
   Future<User?> loginWithEmailAndPassword(String email, String password) async {
     try {
-      final userAuth = await _firebaseAuth
-          .signInWithEmailAndPassword(email: email, password: password);
+      final userAuth = await _firebaseAuth.signInWithEmailAndPassword(
+          email: email, password: password);
       return userAuth.user;
     } catch (e) {
       print(
@@ -44,6 +46,18 @@ class Auth implements AuthBase {
 
   @override
   User? get currentUser => _firebaseAuth.currentUser;
+
+  @override
+  Future<bool> checkEmailExists(String email) async {
+    try {
+      final signInMethods =
+          await _firebaseAuth.fetchSignInMethodsForEmail(email);
+      return signInMethods.isNotEmpty;
+    } catch (error) {
+      print('Error checking email exists: $error');
+      rethrow;
+    }
+  }
 
   @override
   Future<void> logout() async => await _firebaseAuth.signOut();
