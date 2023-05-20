289b69503652aae96b00f6879438025545c0dd90
Mohamed abo el maaty zaky <98196426+MohamedAboElM3aTy@users.noreply.github.com>
2023-05-20 06:53:51 +0300

    Wrapping with bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'package:wed_plan_project/features/cart/data/adapaters/order_model.dart';
+import 'package:wed_plan_project/features/cart/persentation/cubit/cart_bloc.dart';
+import 'package:wed_plan_project/features/favourites/data/adapaters/vendor_model.dart';
+import 'package:wed_plan_project/features/favourites/persentation/cubit/favourite_bloc.dart';
 import 'package:wed_plan_project/utilities/theme_light.dart';
 import 'package:wed_plan_project/utilities/router.dart';
 import 'package:wed_plan_project/utilities/routes.dart';
 
-Future<void> main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await Hive.initFlutter();
+  Hive.registerAdapter(CartItemAdapter());
+  Hive.registerAdapter(VendorAdapter());
   runApp(
-    ProviderScope(
-      child: MyApp(),
-    ),
+    MyApp(),
   );
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'WedPlan',
-      theme: buildTheme(),
-      initialRoute: AppRoutes.welcomeScreen,
-      onGenerateRoute: generateRoute,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => FavouriteBloc()..add(FavouriteLoadEvent()),
+        ),
+        BlocProvider(
+          create: (context) => CartBloc()..add(CartLoadEvent()),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'WedPlan',
+        theme: buildTheme(),
+        initialRoute: AppRoutes.welcomeScreen,
+        onGenerateRoute: generateRoute,
+      ),
     );
   }
 }
-
-// import 'package:wed_plan_project/utilities/dark_theme.dart';
-// theme: buildDarkTheme(),
