90b74866c5e78e2fdc1d648ca6f1d01dc8eabc24
Mohamed abo el maaty zaky <98196426+MohamedAboElM3aTy@users.noreply.github.com>
2023-05-20 06:45:28 +0300

    My Customized Description

---
+++ lib/views/pages/vendor/widgets/description_widget.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:wed_plan_project/models/vendor.dart';
+
+class DescriptionWidget extends StatelessWidget {
+  const DescriptionWidget({
+    super.key,
+    required this.vendor,
+  });
+
+  final Vendor vendor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(6),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Align(
+            alignment: Alignment.topLeft,
+            child: Text(
+              'Description',
+              style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                    fontSize: 26,
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).primaryColor,
+                  ),
+            ),
+          ),
+          const SizedBox(height: 4),
+          Text(
+            'Hello I am ${vendor.name}, I am a ${vendor.category}, I provide many services you can check it out on my instagram page and you will Find examples on my work , I hope it will be helpful to Your needs, wish you a happy wedding :)',
+            style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 18,
+                ),
+          ),
+          Align(
+              alignment: Alignment.bottomRight,
+              child: Container(
+                padding: EdgeInsets.all(10),
+                width: MediaQuery.of(context).size.width * 0.5,
+                height: MediaQuery.of(context).size.height * 0.07,
+                child: ElevatedButton.icon(
+                  style: ButtonStyle(
+                    iconSize: MaterialStateProperty.all(30),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                  ),
+                  icon: Icon(Icons.link),
+                  onPressed: () {
+                    launchUrl(
+                      Uri.parse(vendor.businessUrl),
+                    );
+                  },
+                  label: Text(
+                    'Instagram',
+                    style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                        fontSize: 20,
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black),
+                  ),
+                ),
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
