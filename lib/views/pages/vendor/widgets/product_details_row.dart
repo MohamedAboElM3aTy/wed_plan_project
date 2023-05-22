c845b86842393025b756752a4d6ae88efa96b015
Mohamed abo el maaty zaky <98196426+MohamedAboElM3aTy@users.noreply.github.com>
2023-05-22 19:54:12 +0300

    Adding URL launcher to launch call and edit colors

--- lib/views/pages/vendor/widgets/product_details_row.dart
+++ lib/views/pages/vendor/widgets/product_details_row.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:wed_plan_project/models/vendor.dart';
 
 class ProductDetailsRow extends StatelessWidget {
@@ -6,23 +7,36 @@ class ProductDetailsRow extends StatelessWidget {
     super.key,
     required this.vendor,
     required this.name,
-    required this.icon,
-    required this.iconColor,
-    required this.text,
+    this.icon,
+    this.iconColor,
+    this.text,
     required this.nameStyle,
     required this.catStyle,
+    this.thirdIcon,
   });
 
   final Vendor vendor;
   final String name;
-  final IconData icon;
-  final Color iconColor;
-  final String text;
+  final IconData? icon;
+  final Color? iconColor;
+  final String? text;
   final TextStyle nameStyle;
   final TextStyle catStyle;
+  final IconData? thirdIcon;
 
   @override
   Widget build(BuildContext context) {
+    Future<void> _makePhoneCall(String phoneNumber) async {
+      final Uri launchUri = Uri(
+        scheme: 'tel',
+        path: phoneNumber,
+      );
+      if (await canLaunchUrl(launchUri))
+        await launchUrl(launchUri);
+      else
+        throw 'Could not launch ${launchUri.toString()}';
+    }
+
     return Padding(
       padding: const EdgeInsets.all(5.0),
       child: Row(
@@ -34,19 +48,32 @@ class ProductDetailsRow extends StatelessWidget {
               style: nameStyle,
             ),
           ),
-          Row(
-            children: [
-              Icon(
-                icon,
-                color: iconColor,
-              ),
-              SizedBox(width: 5),
-              Text(
-                text.toString(),
-                style: catStyle,
-              ),
-            ],
-          ),
+          if (text != null)
+            Row(
+              children: [
+                if (thirdIcon != null) ...[
+                  InkWell(
+                    onTap: () => _makePhoneCall(vendor.phoneNumber),
+                    child: Icon(
+                      thirdIcon,
+                      size: 24,
+                      color: Colors.green,
+                    ),
+                  ),
+                  SizedBox(width: 6),
+                ],
+                Icon(
+                  icon,
+                  color: iconColor,
+                  size: 28,
+                ),
+                SizedBox(width: 5),
+                Text(
+                  text.toString(),
+                  style: catStyle,
+                ),
+              ],
+            ),
         ],
       ),
     );
