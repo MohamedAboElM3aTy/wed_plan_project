4fc9476d896682b5e4bcc4166b874ab725768194
Mohamed abo el maaty zaky <98196426+MohamedAboElM3aTy@users.noreply.github.com>
2023-05-22 19:58:55 +0300

    Finialized Vendors Details Screen

--- lib/views/pages/vendor/Screens/vendor_details.dart
+++ lib/views/pages/vendor/Screens/vendor_details.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:wed_plan_project/Providers/cart_provider.dart';
 
+import 'package:wed_plan_project/models/cart.dart';
 import 'package:wed_plan_project/models/vendor.dart';
 import 'package:wed_plan_project/features/cart/presentation/cubit/cart_bloc.dart';
 import 'package:wed_plan_project/features/favourites/presentation/cubit/favourite_bloc.dart';
@@ -37,6 +37,8 @@ class _VendorsDetailsState extends State<VendorsDetails> {
 
   @override
   Widget build(BuildContext context) {
+    final themes = Theme.of(context).textTheme;
+    final snackBar = ScaffoldMessenger.of(context);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).primaryColor,
@@ -48,9 +50,10 @@ class _VendorsDetailsState extends State<VendorsDetails> {
             listener: (context, state) {
               if (state is AddToFavoriteState) {
                 _isFavorite = !_isFavorite;
-                ScaffoldMessenger.of(context).clearSnackBars();
-                ScaffoldMessenger.of(context).showSnackBar(
+                snackBar.clearSnackBars();
+                snackBar.showSnackBar(
                   SnackBar(
+                    duration: const Duration(seconds: 1),
                     content: Text(
                       _isFavorite
                           ? 'Added to favorites!'
@@ -85,153 +88,153 @@ class _VendorsDetailsState extends State<VendorsDetails> {
           ),
         ],
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(25.0),
-              bottomRight: Radius.circular(25.0),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(25.0),
+                bottomRight: Radius.circular(25.0),
+              ),
+              child: Image.asset(
+                widget.vendor.imageUrl,
+                height: 300,
+                width: double.infinity,
+                fit: BoxFit.fill,
+              ),
             ),
-            child: Image.asset(
-              widget.vendor.imageUrl,
-              height: 300,
-              width: double.infinity,
-              fit: BoxFit.fill,
+            const SizedBox(height: 20.0),
+            ProductDetailsRow(
+              vendor: widget.vendor,
+              name: widget.vendor.name.toUpperCase(),
+              nameStyle: themes.titleLarge!.copyWith(
+                color: Colors.black,
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+              catStyle: themes.bodyMedium!.copyWith(
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+                fontSize: 18,
+              ),
             ),
-          ),
-          const SizedBox(height: 6.0),
-          ProductDetailsRow(
-            vendor: widget.vendor,
-            name: widget.vendor.category.toUpperCase(),
-            nameStyle: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 24,
-                ),
-            icon: Icons.star,
-            iconColor: Colors.yellow,
-            text: widget.vendor.rating.toString(),
-            catStyle: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 22,
-                ),
-          ),
-          ProductDetailsRow(
-            vendor: widget.vendor,
-            name: widget.vendor.name.toUpperCase(),
-            nameStyle: Theme.of(context).textTheme.titleLarge!.copyWith(
-                  color: Theme.of(context).primaryColor,
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
-            icon: Icons.phone,
-            iconColor: Colors.green,
-            text: widget.vendor.phoneNumber.toString(),
-            catStyle: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18,
-                ),
-          ),
-          const SizedBox(height: 6.0),
-          DescriptionWidget(vendor: widget.vendor),
-          const SizedBox(height: 8.0),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Expanded(
-                      child: SizedBox(
-                        height: 50,
-                        child: DropDownMenuComponent(
-                          items: const [
-                            'Wedding',
-                            'Engagement',
-                            'Wedding hall',
-                            'Open Hall',
-                            'In Door',
-                            'Out Door',
-                            'In Door & Out Door',
-                          ],
-                          hint: 'Choose Package',
-                          selectedValue: _selectedPackage,
-                          onChanged: (String? newValue) {
-                            _selectedPackage = newValue;
-                          },
+            ProductDetailsRow(
+              vendor: widget.vendor,
+              name: widget.vendor.category.toUpperCase(),
+              nameStyle: themes.bodyMedium!.copyWith(
+                color: Colors.grey,
+                fontSize: 18,
+              ),
+              thirdIcon: Icons.phone,
+              icon: Icons.star,
+              iconColor: Colors.yellow,
+              text: widget.vendor.rating.toString(),
+              catStyle: themes.bodyMedium!.copyWith(
+                fontWeight: FontWeight.w600,
+                color: Colors.grey,
+                fontSize: 20,
+              ),
+            ),
+            const SizedBox(height: 6.0),
+            DescriptionWidget(vendor: widget.vendor),
+            const SizedBox(height: 8.0),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                        child: SizedBox(
+                          height: 50,
+                          child: DropDownMenuComponent(
+                            items: const [
+                              'WEDDING',
+                              'ENGAGEMENT',
+                            ],
+                            hint: 'Choose Package',
+                            selectedValue: _selectedPackage,
+                            onChanged: (String? newValue) {
+                              _selectedPackage = newValue;
+                            },
+                          ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(height: 16.0),
-          ProductDetailsRow(
-            vendor: widget.vendor,
-            name: 'Price',
-            icon: Icons.attach_money,
-            iconColor: Colors.white10,
-            text: '\$${widget.vendor.price.toString()}',
-            nameStyle: Theme.of(context).textTheme.titleLarge!.copyWith(
-                  color: Theme.of(context).primaryColorDark,
-                  fontSize: 26,
-                  fontWeight: FontWeight.bold,
-                ),
-            catStyle: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 24,
-                  color: Colors.red,
-                ),
-          ),
-          const SizedBox(height: 12.0),
-          BlocConsumer<CartBloc, CartState>(
-            listener: (context, state) {
-              if (state is AddToCartState) {
-                _isAddedToCart = !_isAddedToCart;
-                ScaffoldMessenger.of(context).clearSnackBars();
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text(
-                      _isAddedToCart ? 'Added to cart!' : 'Removed from cart!',
-                    ),
+                    ],
                   ),
-                );
-              }
-            },
-            builder: (context, state) {
-              return MainButton(
-                text: _isAddedToCart ? 'Remove From Cart' : 'Add to cart',
-                onTap: () {
-                  if (_selectedPackage == null) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        content: Text(
-                          'Please choose a package',
-                        ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            ProductDetailsRow(
+              vendor: widget.vendor,
+              name: 'Price',
+              icon: Icons.attach_money,
+              iconColor: Colors.white10,
+              text: '\$${widget.vendor.price.toString()}',
+              nameStyle: themes.titleLarge!.copyWith(
+                color: Colors.black,
+                fontSize: 22,
+              ),
+              catStyle: themes.bodyMedium!.copyWith(
+                fontWeight: FontWeight.bold,
+                fontSize: 22,
+                color: Colors.red[700],
+              ),
+            ),
+            const SizedBox(height: 12.0),
+            BlocConsumer<CartBloc, CartState>(
+              listener: (context, state) {
+                if (state is AddToCartState) {
+                  _isAddedToCart = !_isAddedToCart;
+                  snackBar.clearSnackBars();
+                  snackBar.showSnackBar(
+                    SnackBar(
+                      duration: const Duration(seconds: 1),
+                      content: Text(
+                        _isAddedToCart
+                            ? 'Added to cart!'
+                            : 'Removed from cart!',
                       ),
-                    );
-                    return;
-                  }
-                  final item = CartItem(
-                    vendor: widget.vendor,
-                    price: widget.vendor.price,
-                    id: widget.vendor.id,
-                    packageName: _selectedPackage!,
+                    ),
                   );
-                  if (_isAddedToCart)
-                    _cartBloc.add(CartRemoveEvent(item));
-                  else
-                    _cartBloc.add(CartAddEvent(item));
-                },
-                hasCircularBorder: true,
-              );
-            },
-          ),
-        ],
+                }
+              },
+              builder: (context, state) {
+                return MainButton(
+                  text: _isAddedToCart ? 'Remove From Cart' : 'Add to cart',
+                  onTap: () {
+                    if (_selectedPackage == null) {
+                      snackBar.showSnackBar(
+                        SnackBar(
+                          duration: const Duration(seconds: 1),
+                          content: const Text(
+                            'Please choose a package',
+                          ),
+                        ),
+                      );
+                      return;
+                    }
+                    final item = CartItem(
+                      vendor: widget.vendor,
+                      price: widget.vendor.price,
+                      id: widget.vendor.id,
+                      packageName: _selectedPackage!,
+                    );
+                    if (_isAddedToCart)
+                      _cartBloc.add(CartRemoveEvent(item));
+                    else
+                      _cartBloc.add(CartAddEvent(item));
+                  },
+                  hasCircularBorder: true,
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
